b30bc18c076a37ab3c08468dab3c48fa655e7b9a
Pinny Geliebter <pgibralter@gmail.com>
2021-06-18 09:45:09 -0400

    adds all movies route and controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,5 @@
 class MoviesController < ApplicationController
+  def all_movies
+    render json: Movie.all.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/1_actor" => "actors#actor_1"
+  get "/movies/all" => "movies#all_movies"
+  get "/movies/:id" => "movies#one_movie"
 end
