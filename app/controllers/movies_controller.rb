b8e763fa074e4df4b499203565def72ed8ec95f1
Pinny Geliebter <pgibralter@gmail.com>
2021-06-18 09:52:39 -0400

    adds one movie route and controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,9 @@ class MoviesController < ApplicationController
   def all_movies
     render json: Movie.all.as_json
   end
+
+  def one_movie
+    input = params["id"]
+    render json: Movie.find_by(id: input).as_json
+  end
 end
