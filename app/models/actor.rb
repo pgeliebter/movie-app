08863bbf46940544c9903b583ccdd3d3fbc673f9
Pinny Geliebter <pgibralter@gmail.com>
2021-06-24 22:56:12 -0400

    adds validations to Movie and adds inclusion for known_for in actor

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,5 @@
 class Actor < ApplicationRecord
   validates :first_name, :last_name, length: { minimum: 2 }
   validates :age, inclusion: { in: 13.. }
+  validates :known_for, inclusion: { in: Movie.pluck(:title) }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :year, inclusion: { in: 1900..2025 }
 end
