570773db662bfb23f7d3fe578dffee1795193b21
Pinny Geliebter <pgibralter@gmail.com>
2021-06-28 23:12:36 -0400

    adds serialzier gem and files for actor and movie

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,4 +2,5 @@ class Actor < ApplicationRecord
   validates :first_name, :last_name, length: { minimum: 2 }
   validates :age, inclusion: { in: 13.. }
   validates :known_for, inclusion: { in: Movie.pluck(:title), message: "Movie does not exist in database" }
+  belongs_to :movie
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
   validates :title, presence: true
   validates :year, numericality: { less_than_or_equal_to: 2025, greater_than_or_equal_to: 1901 }
+
+  has_many :actors
 end
